dc4d5cbf290639514772ef16d9a2ceeba8c4825c
Reyner <reyenr1012002@gmail.com>
2023-11-15 13:46:18 -0600

    Added js

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-window.addEventListener('scroll', function(){
+ // Este codigo es el header, stikcy, y cambio de imagen usando Listener de scroll y if/else con ? y :
+
+window.addEventListener('scroll', function(){                         
     var header = document.querySelector("header");
     header.classList.toggle('sticky', window.scrollY > 0);
+    var logox = document.getElementById('logox');
+    window.addEventListener('scroll', function() {
+        logox.src = window.scrollY > 0 ? 'logox.png' : 'logo1.png';
+    });
 });
+
+// Responsive menu de navegacion abrir y cerrar
+
+var menu = document.querySelector('.menu-pc');
+var menuBtn = document.querySelector('.menu-btn');
+var closeBtn = document.querySelector('.close-btn');
+
+menuBtn.addEventListener("click", ()=>{
+    menu.classList.add('active');
+});
+
